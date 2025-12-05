d4e1e85f22867ca2f037da8b55ccb48e05353bf1
BenGrainger <bfgrainger@gmail.com>
2025-12-05 15:09:56 +0000

    moved inference to legacy

--- scripts/train_segmentation.py
+++ scripts/train_segmentation.py
@@ -15,7 +15,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torch import nn, optim
 from mask_unet.models import get_segmentation_model
-from mask_unet.training.train_segmentor import train_segmentator
+from mask_unet.training.train_segmentor import train_segmentor
 
 
 def get_transforms():
@@ -27,20 +27,23 @@ def get_transforms():
         ToTensorV2()
     ])
 
-train_dir = "debris_data/train/"
-val_dir = "debris_data/val/"
+criterion = nn.BCEWithLogitsLoss() 
+
+train_dir = "/ceph/zoo/users/debris/Mask_training_script_Unet/debris_data/train/"
+val_dir = "/ceph/zoo/users/debris/Mask_training_script_Unet/debris_data/val/"
+
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print('using', DEVICE)
 
+
 dataset = SegmentationDataset(train_dir, transforms=get_transforms())
 train_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+
 validation = SegmentationDataset(val_dir, transforms=get_transforms())
 val_loader = DataLoader(validation, batch_size=4, shuffle=True)
 
-model = get_segmentation_model().to(device)
-
-criterion = nn.BCEWithLogitsLoss()  # <-- ORIGINAL (binary)
+model = get_segmentation_model().to(DEVICE)
 
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
@@ -48,7 +51,7 @@ num_epochs = 5
 
 model_save_path = "models/best_debris_segmentation_50.pth"
 
-train_segmentator(model, optimizer, train_loader, val_loader, criterion, DEVICE, num_epochs, model_save_path)
+train_segmentor(model, optimizer, train_loader, val_loader, criterion, DEVICE, num_epochs, model_save_path)
 
 torch.save(model.state_dict(), "models/final_debris_segmentation_50.pth")
 
